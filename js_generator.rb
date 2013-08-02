3510f428f9f9a7d57a5d7ccf0414001512a20c73
macournoyer <macournoyer@gmail.com>
2013-08-02 09:46:51 -0400

    Start implementing JS code generator.

--- js_generator.rb
+++ js_generator.rb
@@ -5,23 +5,28 @@ class JsGenerator
   end
   
   def compile_all(nodes)
-    
+    nodes.each do |node|
+      node.compile(self)
+      emit ";\n"
+    end
   end
   
   def number_literal(value)
-    
+    emit value # 1 => 1
   end
   
   def string_literal(value)
-    
+    emit "\"" + value + "\"" # "value"
   end
   
   def set_local(name, value)
-    
+    @locals << name unless @locals.include?(name)
+    emit "#{name} = "
+    value.compile(self)
   end
   
   def get_local(name)
-    
+    emit name
   end
   
   def if(condition, body, else_body)
@@ -35,6 +40,7 @@ class JsGenerator
   
   # Called at the end of compilation to assemble all the code generated.
   def assemble
-    @code.join
+    # var a, b, c; <= all @locals
+    "var " + @locals.join(", ") + ";\n" + @code.join
   end
 end
