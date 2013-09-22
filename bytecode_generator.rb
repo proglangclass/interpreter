500f960295d84ef719e949b6281836d070644c49
macournoyer <macournoyer@gmail.com>
2013-09-22 14:59:45 -0700

    Compile to bytecode.

--- bytecode_generator.rb
+++ bytecode_generator.rb
@@ -16,6 +16,9 @@ ADD,             #              [a, b]         [result]
 RETURN = *       #              []             []
 (0..11)
 
+# CALL = 0
+# PUSH_NUMBER = 1
+
 class BytecodeGenerator
   def initialize
     @literals = []
@@ -24,31 +27,40 @@ class BytecodeGenerator
   end
   
   def compile_all(nodes)
-    
+    nodes.each do |node|
+      node.compile(self)
+    end
   end
   
   def number_literal(value)
-    
+    # literal_index(1000) => 0
+    # literal_index(1001) => 1
+    # literal_index(1000) => 0
+    emit PUSH_NUMBER, literal_index(value)
   end
   
   def string_literal(value)
-    
+    # literal_index("hi") => 0
+    # literal_index("bye") => 1
+    # literal_index("hi") => 0
+    emit PUSH_STRING, literal_index(value)
   end
   
   def true_literal
-    
+    emit PUSH_BOOL, 1
   end
   
   def false_literal
-    
+    emit PUSH_BOOL, 0
   end
   
   def nil_literal
-    
+    emit PUSH_NIL
   end
   
   def set_local(name, value)
-    
+    value.compile(self)
+    emit SET_LOCAL, local_index(name)
   end
   
   def get_local(name)
@@ -56,11 +68,31 @@ class BytecodeGenerator
   end
   
   def call(receiver, method, arguments)
-    
+    if receiver
+      receiver.compile(self)
+    else # print("hi")
+      emit PUSH_SELF
+    end
+
+    arguments.each do |arg|
+      arg.compile(self)
+    end
+
+    # Static Typing: int a;
+    if method == "+"
+      emit ADD
+      return
+    end
+
+    emit CALL, literal_index(method), arguments.size
   end
   
   def if(condition, body, else_body)
-    
+    condition.compile(self)
+    emit JUMP_UNLESS, 0
+    offset_index = @instructions.size - 1
+    body.compile(self)
+    @instructions[offset_index] = @instructions.size - 1 - offset_index
   end
   
   # Returns the index of the local in the local table
