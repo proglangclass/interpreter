6708f55a8387d2307b2ca914cb9068826940ae5f
macournoyer <macournoyer@gmail.com>
2013-12-11 12:33:09 -0500

    Start implement a JS compiler

--- js_generator.rb
+++ js_generator.rb
@@ -3,25 +3,36 @@ class JsGenerator
     @code = []
     @locals = []
   end
+
+  # 1;
+  # function() { ... };
   
   def compile_all(nodes)
-    
+    nodes.each do |node|
+      node.compile(self)
+      emit ";\n"
+    end
   end
   
   def number_literal(value)
-    
+    emit value
   end
   
   def string_literal(value)
-    
+    emit '"' + value + '"'
   end
   
+  # In Javascript:
+  # var a, b;
+  # a = 1;
   def set_local(name, value_node)
-    
+    @locals << name unless @locals.include?(name)
+    emit "#{name} = "
+    value_node.compile(self)
   end
   
   def get_local(name)
-    
+    emit name
   end
   
   def if(condition_node, body_node, else_body_node)
@@ -35,6 +46,6 @@ class JsGenerator
   
   # Called at the end of compilation to assemble all the code generated.
   def assemble
-    @code.join
+    "var " + @locals.join(', ') + ";\n" + @code.join
   end
 end
